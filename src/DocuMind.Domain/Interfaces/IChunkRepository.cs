be666551be99c64109a7a903d58c5ab4e1985c53
Nishanth Rajan <nishanthrjn@gmail.com>
2026-09-02 18:23:43 +0100

    add hybrid search (vector + full-text RRF) to ChunkRepository and migration

--- src/DocuMind.Domain/Interfaces/IChunkRepository.cs
+++ src/DocuMind.Domain/Interfaces/IChunkRepository.cs
@@ -1,11 +1,8 @@
 using DocuMind.Domain.Entities;
-
 namespace DocuMind.Domain.Interfaces;
-
 public interface IChunkRepository
 {
     Task                      SaveChunksAsync(List<DocumentChunk> chunks, CancellationToken ct);
     Task<List<DocumentChunk>> SearchSimilarAsync(float[] queryEmbedding, int topK, CancellationToken ct);
+    Task<List<DocumentChunk>> HybridSearchAsync(float[] queryEmbedding, string queryText, int topK, CancellationToken ct);
 }
-
-
